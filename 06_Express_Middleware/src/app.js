df3bb9cf3a5142e60723c692157096c2f705a5a8
Shubham Yadav <cshubyadav@gmail.com>
2024-11-01 11:36:06 +0530

    Handling Multiple Routes

---
+++ 06_Express_Middleware/src/app.js
@@ -0,0 +1,25 @@
+const express = require('express')
+
+const app = express()
+
+const port = 3000
+
+app.listen(port, () => {
+    console.log("Server Listening at port "+port);
+})
+
+// Handling multiple routes
+app.use("/userLogin",
+    (req,res,next) => {
+        console.log("Genearte JWT Token, Response #1")
+        next()
+    },
+    (req,res,next) => {
+        console.log("Generate Access Tokes, Response #2")
+        next()
+    },
+    (req,res,next) => {
+        console.log("Authenticate the User, Response #3")
+        res.send("Response #3");
+    }
+)
