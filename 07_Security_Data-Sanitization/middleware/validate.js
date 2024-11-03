2fb7e77a43b2c1af933bcb9b3cbaa026153b9219
Shubham Yadav <cshubyadav@gmail.com>
2024-11-03 12:35:45 +0530

    Middleware for Data Validation for /login API

--- 07_Security_Data-Sanitization/middleware/validate.js
+++ 07_Security_Data-Sanitization/middleware/validate.js
@@ -1,39 +1,77 @@
-const validator = require('validator');
-const User = require('../models/user.js')
+const validator = require("validator");
+const User = require("../models/user.js");
 
-function validateSignUp(req,res,next) {
-    const { emailId,password,gender } = req.body;
-    try{
-        if(!emailId || !password || !gender) {
-            console.log("ERROR > middleware/validate.js > /signUp > Mandatory Fields -> 'emailId', 'password', 'gender' is required")
-            res.status(400).send("Mandatory Fields -> 'emailId', 'password', 'gender' is required")
+function validateSignUp(req, res, next) {
+    const { emailId, password, gender } = req.body;
+    try {
+        if (!emailId || !password || !gender) {
+            console.log(
+                "ERROR > middleware/validate.js > /signUp > validateSignUp > Mandatory Fields -> 'emailId', 'password', 'gender' is required"
+            );
+            res.status(400).send(
+                "Mandatory Fields -> 'emailId', 'password', 'gender' is required"
+            );
+        } else if (!validator.isEmail(emailId)) {
+            console.log(
+                "ERROR > middleware/validate.js > /signUp > validateSignUp > Email Id Invalid"
+            );
+            res.status(400).send("Email Id Invalid");
+        } else if (!validator.isStrongPassword(password)) {
+            console.log(
+                "ERROR > middleware/validate.js > /signUp > validateSignUp > Not A Strong Password. Please Enter A New Password"
+            );
+            res.status(400).send(
+                "Not A Strong Password. Please Enter A New Password"
+            );
+        } else if (
+            !["male", "female", "others"].includes(gender.toLowerCase())
+        ) {
+            console.log(
+                "ERROR > middleware/validate.js > /signUp > validateSignUp > Gender should be one of these 'male','female','others'"
+            );
+            res.status(400).send(
+                "Gender should be one of these 'male','female','others'"
+            );
+        } else {
+            next();
         }
-        else if(!validator.isEmail(emailId)){
-            console.log("ERROR > middleware/validate.js > /signUp > Email Id Invalid")
-            res.status(400).send("Email Id Invalid")
-        } else if(!validator.isStrongPassword(password)){
-            console.log("ERROR > middleware/validate.js > /signUp > Not A Strong Password. Please Enter A New Password");
-            res.status(400).send("Not A Strong Password. Please Enter A New Password");
-        } else if(!["male","female","others"].includes(gender.toLowerCase())) {
-            console.log("ERROR > middleware/validate.js > /signUp > Gender should be one of these 'male','female','others'")
-            res.status(400).send("Gender should be one of these 'male','female','others'");
-        }
-        else {
-            next()
-        }
-    } catch(err) {
+    } catch (err) {
         let error = {
             statusCode: 400,
             name: err.name,
-            message: err.message
+            message: err.message,
         };
-        console.log("ERROR > middleware/validate.js > /signUp > Validation Failed");
+        console.log(
+            "ERROR > middleware/validate.js > /signUp > validateSignUp > Validation Failed"
+        );
         console.table(error);
-        res.status(404).send("ERROR > middleware/validate.js > /signUp > "+err.message)
+        res.status(404).send(
+            "ERROR > middleware/validate.js > /signUp > validateSignUp > " +
+                err.message
+        );
     }
-    
+}
+
+function validateLogin(req, res, next) {
+    const { emailId, password } = req.body;
+    if (!emailId || !password) {
+        console.log(
+            "ERROR > middleware/validate.js > /login > validateLogin > Mandatory Fields -> 'emailId', 'password' is required"
+        );
+        res.status(400).send(
+            "Mandatory Fields -> 'emailId', 'password' is required"
+        );
+    } else if (!validator.isEmail(emailId)) {
+        console.log(
+            "ERROR > middleware/validate.js > /login > validateLogin > Email Id Invalid"
+        );
+        res.status(400).send("");
+    } else {
+        next();
+    }
 }
 
 module.exports = {
-    validateSignUp
-}
+    validateSignUp,
+    validateLogin,
+};
--- 07_Security_Data-Sanitization/src/app.js
+++ 07_Security_Data-Sanitization/src/app.js
@@ -1,87 +1,98 @@
-const express = require('express');
-const { dbConnection } = require('../config/database.js');
-const User = require('../models/user.js')
-const bcrypt = require('bcrypt');
-const { validateSignUp } = require('../middleware/validate.js')
+const express = require("express");
+const { dbConnection } = require("../config/database.js");
+const User = require("../models/user.js");
+const bcrypt = require("bcrypt");
+const { validateSignUp, validateLogin } = require("../middleware/validate.js");
 
 const app = express();
 
 const port = 3000;
 
-( async () => {
-    await dbConnection()
+(async () => {
+    await dbConnection();
     try {
         app.listen(port, () => {
-            console.log("Server Listening to Port "+port);
-        })
+            console.log("Server Listening to Port " + port);
+        });
         console.log("SUCCESS > src/app.js > Express Server Initialized");
     } catch (err) {
         console.log("ERROR > src/app.js > Express Server Not Initialized");
     }
-}
-)();
+})();
 
+app.use(express.json());
 
-app.use(express.json())
-
-app.post('/signUp', validateSignUp, async (req,res,next) => {
+app.post("/signUp", validateSignUp, async (req, res, next) => {
     try {
-        const user = await User.findOne({emailId: req.body.emailId});
-        if(!user){
-            let hashedPassword = await bcrypt.hash(req.body.password,10);
+        const user = await User.findOne({ emailId: req.body.emailId });
+        if (!user) {
+            let hashedPassword = await bcrypt.hash(req.body.password, 10);
             let userDetails = new User({
                 emailId: req.body.emailId,
                 password: hashedPassword,
                 firstName: req.body.firstName,
                 lastName: req.body.lastName,
                 age: req.body.age,
-                gender: req.body.gender
+                gender: req.body.gender,
             });
             let response = await userDetails.save();
-            console.log("SUCCESS > src/app.js > /signUp > User Data Inserted Successfully");
+            console.log(
+                "SUCCESS > src/app.js > /signUp > User Data Inserted Successfully"
+            );
             res.send(response);
         } else {
-            console.log("ERROR > src/app.js > /signUp > EmailId Already Exists");
-            res.status(400).send("EmailId Already Exists")
+            console.log(
+                "ERROR > src/app.js > /signUp > EmailId Already Exists"
+            );
+            res.status(400).send("EmailId Already Exists");
         }
-            
     } catch (err) {
         let error = {
             statusCode: 400,
             name: err.name,
-            message: err.message
+            message: err.message,
         };
-        console.log("ERROR > src/app.js > /signUp > User Data Insertion Failed");
+        console.log(
+            "ERROR > src/app.js > /signUp > User Data Insertion Failed"
+        );
         console.table(error);
-        res.status(400).send("ERROR > src/app.js > /signUp > User Data Insertion Failed");
+        res.status(400).send(
+            "ERROR > src/app.js > /signUp > User Data Insertion Failed"
+        );
     }
-})
+});
 
-
-app.post('/login', async (req,res) => {
-    try{
-        const user = await User.findOne({emailId: req.body.emailId});
-        if(!user){
-            console.log("ERROR > src/app.js > /login > User Not Found")
+app.post("/login", validateLogin, async (req, res) => {
+    try {
+        const user = await User.findOne({ emailId: req.body.emailId });
+        if (!user) {
+            console.log("ERROR > src/app.js > /login > User Not Found");
             res.status(404).send("User Not Found");
         } else {
-            const isPasswordMatched = await bcrypt.compare(req.body.password, user.password);
-            if(isPasswordMatched){
-                console.log("SUCCESS > src/app.js > /login > Data Fetched Successfully")
+            const isPasswordMatched = await bcrypt.compare(
+                req.body.password,
+                user.password
+            );
+            if (isPasswordMatched) {
+                console.log(
+                    "SUCCESS > src/app.js > /login > Data Fetched Successfully"
+                );
                 res.send(user);
             } else {
-                console.log("ERROR > src/app.js > /login > Invalid Credentials")
+                console.log(
+                    "ERROR > src/app.js > /login > Invalid Credentials"
+                );
                 res.status(404).send("Invalid Credentials");
             }
         }
-    } catch(err) {
+    } catch (err) {
         console.log("ERROR > src/app.js > /login > Data Fetch Failed");
         let error = {
             statusCode: 400,
             name: err.name,
-            message: err.message
+            message: err.message,
         };
-        console.table(error)
-        res.status(400).send("ERROR > src/app.js > /login > "+err.message);
+        console.table(error);
+        res.status(400).send("ERROR > src/app.js > /login > " + err.message);
     }
-})
+});
